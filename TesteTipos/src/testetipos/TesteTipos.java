c8737a2e931e58771a4711c412f315e804d96d36
carolina-dos-anjos <102593235+carolanjos@users.noreply.github.com>
2022-08-09 09:02:29 -0300

    Add files via upload

---
+++ TesteTipos/src/testetipos/TesteTipos.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testetipos;
+
+/**
+ *
+ * @author Suporte
+ */
+public class TesteTipos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String valor = "30.5";
+        float idade = Float.parseFloat(valor);
+        System.out.printf("%.3f", idade);
+    }
+    
+}
